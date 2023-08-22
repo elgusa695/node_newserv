646fa4ccae652e678319bc2a482c04819c3e64b6
elgusa695 <142939515+elgusa695@users.noreply.github.com>
2023-08-22 15:24:56 -0500

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,7 @@
 /**
  * CONFIGURACION 
  */
-    const API_URL = 'http://127.0.0.1:8080'; // Cambiar según convenga.
+    const API_URL = 'https://apiownerservi.cleverapps.io'; // Cambiar según convenga.
     // const API_URL = 'https://apinewentrega.cleverapps.io'; // Cambiar según convenga.
 
 
