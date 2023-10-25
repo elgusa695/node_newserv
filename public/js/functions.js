bb053d1d3087262c1071ce4cf7c1ba7802638354
elgusa695 <142939515+elgusa695@users.noreply.github.com>
2023-10-24 22:34:30 -0500

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,7 @@
 /**
  * CONFIGURACION 
  */
-    const API_URL = 'https://d2a2x6lktdvk4b.cloudfront.net'; // Cambiar según convenga.
+    const API_URL = 'https://d1jmol6otohf96.cloudfront.net'; // Cambiar según convenga.
     // const API_URL = 'https://apinewentrega.cleverapps.io'; // Cambiar según convenga.
 
 
